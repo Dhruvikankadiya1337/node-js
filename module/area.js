30bd73499ab25a07d2cca877b8798553369425f9
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-06 21:08:20 +0530

    first commit

---
+++ module/area.js
@@ -0,0 +1,11 @@
+const {
+  areaOfTriangle,
+  areaOfCircle,
+  areaOfSquare,
+  areaOfRectengle,
+} = require("./math.js");
+
+areaOfTriangle(10, 20);
+areaOfCircle(10);
+areaOfSquare(10);
+areaOfRectengle(20, 10);
