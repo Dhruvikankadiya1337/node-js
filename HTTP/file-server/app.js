fa7c7fef42dbe767abe0337b35a47563df43d53d
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-12 09:10:57 +0530

    update

--- HTTP/file-server/app.js
+++ HTTP/file-server/app.js
@@ -2,37 +2,36 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 5000;
-
-function servePage(res, pageName) {
-  const filePath = path.join(__dirname, "pages", pageName);
+function showPage(res, fileName) {
+  const filePath = path.join(__dirname, "pages", fileName);
 
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      res.writeHead(500, { "Content-Type": "text/html" });
-      return res.end("Server Error");
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("<h1>404 - Page Not Found </h1>");
     }
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
 }
 
+
 const server = http.createServer((req, res) => {
+  
+  console.log("User visited:", req.url);
 
-  const logMessage = req.url + " visited\n";
-  fs.appendFile(path.join(__dirname, "logs", "server.log"), logMessage, () => {});
-
-  if (req.url === "/") {
-    servePage(res, "index.html");
-  } else if (req.url === "/about") {
-    servePage(res, "about.html");
-  } else if (req.url === "/contact") {
-    servePage(res, "contact.html");
+  if (req.url === "/" || req.url === "/home.html") {
+    showPage(res, "home.html");
+  } else if (req.url === "/about" || req.url === "/about.html") {
+    showPage(res, "about.html");
+  } else if (req.url === "/contact" || req.url === "/contact.html") {
+    showPage(res, "contact.html");
   } else {
-    servePage(res, "error.html");
+    showPage(res, "error.html");
   }
 });
 
-server.listen("5000", () => {
-  console.log("✅ Server running at http://localhost:5000");
+server.listen(5000, () => {
+  console.log("Server running at http://localhost:5000");
 });
