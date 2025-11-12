5b08639cf469144e481b1762b4009a72798fe3fb
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-12 09:26:29 +0530

    update

--- HTTP/file-server/app.js
+++ HTTP/file-server/app.js
@@ -2,33 +2,32 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-function showPage(res, fileName) {
-  const filePath = path.join(__dirname, "pages", fileName);
+function loadPage(res, pageName) {
+  const fullPath = path.join(__dirname, "pages", pageName);
 
-  fs.readFile(filePath, "utf8", (err, data) => {
+  fs.readFile((err, data) => {
     if (err) {
       res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end("<h1>404 - Page Not Found </h1>");
+      res.end("<h1>Page Not Found</h1>");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
     }
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
   });
 }
 
-
 const server = http.createServer((req, res) => {
-  
-  console.log("User visited:", req.url);
-
-  if (req.url === "/" || req.url === "/home.html") {
-    showPage(res, "home.html");
-  } else if (req.url === "/about" || req.url === "/about.html") {
-    showPage(res, "about.html");
-  } else if (req.url === "/contact" || req.url === "/contact.html") {
-    showPage(res, "contact.html");
-  } else {
-    showPage(res, "error.html");
+  if (req.url === "/") {
+    loadPage(res, "home.html");
+  } 
+  else if (req.url === "/about") {
+    loadPage(res, "about.html");
+  } 
+  else if (req.url === "/contact") {
+    loadPage(res, "contact.html");
+  } 
+  else {
+    loadPage(res, "404.html");
   }
 });
 
