ea6a90572775fa00b22df404a3e48780218b46f4
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-12 22:04:38 +0530

    update

--- HTTP/file-server/app.js
+++ HTTP/file-server/app.js
@@ -2,35 +2,46 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-function loadPage(res, pageName) {
-  const fullPath = path.join(__dirname, "pages", pageName);
+const server = http.createServer((req, res) => {
+  console.log("Request URL:", req.url);
 
-  fs.readFile((err, data) => {
+
+  if (req.url === "/jsondata") {
+    const data = {
+      BookName: "Atomic Habits",
+      price: 5.89,
+      rating: "4.5 / 5",
+      inStock: true,
+    };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+    return;
+  }
+
+  let filepath;
+
+  if (req.url === "/" || req.url === "/home") {
+    filepath = path.join(__dirname, "home.html");
+  } else if (req.url === "/about") {
+    filepath = path.join(__dirname, "about.html");
+  } else if (req.url === "/contact") {
+    filepath = path.join(__dirname, "contact.html");
+  } else {
+    filepath = path.join(__dirname, "404.html");
+  }
+
+
+  fs.readFile(filepath, (err, data) => {
     if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end("<h1>Page Not Found</h1>");
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Server Error");
     } else {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     }
   });
-}
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    loadPage(res, "home.html");
-  } 
-  else if (req.url === "/about") {
-    loadPage(res, "about.html");
-  } 
-  else if (req.url === "/contact") {
-    loadPage(res, "contact.html");
-  } 
-  else {
-    loadPage(res, "404.html");
-  }
 });
 
 server.listen(5000, () => {
-  console.log("Server running at http://localhost:5000");
+  console.log("Server started on http://localhost:5000");
 });
