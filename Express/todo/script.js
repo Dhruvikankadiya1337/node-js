71b7fd5c7e5788f677496b38bc2fb2c4390f27c5
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-18 09:38:46 +0530

    18/11/25

---
+++ Express/todo/script.js
@@ -0,0 +1,85 @@
+const todosDiv = document.getElementById("todos");
+const input = document.getElementById("todoInput");
+
+
+function loadTodos() {
+  fetch("http://localhost:4000/todos")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      todosDiv.innerHTML = "";
+
+     
+      data.map(function(item) {
+        const div = document.createElement("div");
+        div.className = "todo";
+
+        div.innerHTML = `
+          <span>${item.task}</span>
+          <div>
+            <button onclick="editTodo(${item.id}, '${item.task}')">Edit</button>
+            <button onclick="deleteTodo(${item.id})">Delete</button>
+          </div>
+        `;
+
+        todosDiv.appendChild(div);
+      });
+    })
+    .catch(function(error) {
+      console.log("Error loading todos:", error);
+    });
+}
+
+
+function addTodo() {
+  const text = input.value.trim();
+  if (text === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  fetch("http://localhost:4000/todos", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: text })
+  })
+  .then(function() {
+    input.value = "";
+    loadTodos();
+  })
+  .catch(function(error) {
+    console.log("Error adding todo:", error);
+  });
+}
+
+function editTodo(id, oldTask) {
+  const newTask = prompt("Edit task:", oldTask);
+  if (newTask === null || newTask.trim() === "") return;
+
+  fetch(`http://localhost:4000/todos/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: newTask })
+  })
+  .then(function() {
+    loadTodos();
+  })
+  .catch(function(error) {
+    console.log("Error editing todo:", error);
+  });
+}
+
+function deleteTodo(id) {
+  fetch(`http://localhost:4000/todos/${id}`, {
+    method: "DELETE"
+  })
+  .then(function() {
+    loadTodos();
+  })
+  .catch(function(error) {
+    console.log("Error deleting todo:", error);
+  });
+}
+
+loadTodos();
