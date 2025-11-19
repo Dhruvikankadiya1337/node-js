2cdac84ee4f537d1a7a7f8137eda3eb522db6c3d
Dhruvikankadiya1337 <dhruvikankadiy@gmail.com>
2025-11-19 17:17:46 +0530

    update

--- Express/todo/backend/app.js
+++ Express/todo/backend/app.js
@@ -1,21 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "../frontend")));
 
+
 let todos = [
-  { id: 1, text: "Learn Node.js" },
-  { id: 2, text: "Practice JavaScript" }
+  { id: 1, 
+    text: "Learn Node.js",
+  },
+  { id: 2,
+    text: "Practice express js",
+   },
 ];
 
-
-app.get("/todos", (req, res) => res.json(todos));
+app.get("/todos", (req, res) => 
+  res.json(todos));
 
 app.post("/todos", (req, res) => {
   const newTodo = { id: Date.now(), text: req.body.text };
